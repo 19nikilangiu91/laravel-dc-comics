dde39b9e4c037c85ccb3fd48b8334a6c2691b4c7
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-06 16:29:57 +0100

    Created Home function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Importo Il model "Person"
+
+use App\Models\Person;
+
 class MainController extends Controller
 {
+
     public function home()
     {
+        $people = Person::all();
 
-        return view('pages.home');
+        return view('pages.home', compact('people'));
     }
 }
