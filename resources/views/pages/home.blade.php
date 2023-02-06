7697bfc31c382f7775ba08b9f3e2bf51f3c12791
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-06 16:30:36 +0100

    Created foreach

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     
-    <h1>CONTENT</h1>
+    <h1>People</h1>
+    <ul>
+        @foreach ($people as $person)
+            <li>
+                First Name: {{ $person -> first_name }}<br>
+                Last Name: {{ $person -> last_name }}<br>
+                Date of Birth: {{ $person -> date_of_birth }}<br>
+                Height: {{ $person -> height }} cm <br>
+                <br>
+            </li>
+        @endforeach
+    </ul>
 
 @endsection
